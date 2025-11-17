507a0a81ca35982be2eb7cedf673546976e817a3
castropatrick <patrickcastro73@gmail.com>
2025-11-17 16:43:00 -0300

    Criando arquivo de cores para o projeto

---
+++ constants/Colors.jsx
@@ -0,0 +1,11 @@
+export const Colors = {
+  primary: '#0066CC',      
+  secondary: '#00CC88',    
+  background: '#F5F5F5',   
+  white: '#FFFFFF',
+  text: '#333333',
+  textLight: '#666666',
+  error: '#FF4444',
+  success: '#00CC88',
+  border: '#DDDDDD',
+};
